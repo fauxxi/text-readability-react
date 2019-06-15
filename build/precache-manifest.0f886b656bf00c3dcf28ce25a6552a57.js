229c24ad58b468391e81454698a0b9808f80fd5f
fauxxi <fauxxi.as@gmail.com>
2019-06-15 11:35:15 +0200

    built successed, but api has corb issue

---
+++ build/precache-manifest.0f886b656bf00c3dcf28ce25a6552a57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac517adf44541523cabdedd13e8c6855",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19d93f44be7872b1576e",
+    "url": "/static/css/2.4223df75.chunk.css"
+  },
+  {
+    "revision": "2ca3128bd66926147c33",
+    "url": "/static/css/main.ad3afd6b.chunk.css"
+  },
+  {
+    "revision": "19d93f44be7872b1576e",
+    "url": "/static/js/2.18777b93.chunk.js"
+  },
+  {
+    "revision": "2ca3128bd66926147c33",
+    "url": "/static/js/main.f70f237b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8f70d48881bdc0b561788d5973b83de4",
+    "url": "/static/media/education.8f70d488.svg"
+  }
+]);
